e33ea23082b469f49372999b7e6b0d4430f22745
jpaulobf <jpaulobf@gmail.com>
2024-08-11 11:37:37 +0100

    MVC to CleanArch update - Application YAML

--- demo/src/main/java/com/example/demo/mvc/model/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.mvc.model.repository;
-
-import com.example.demo.mvc.model.dto.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
