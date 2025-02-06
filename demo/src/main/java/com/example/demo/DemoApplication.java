0e03c69a21f7fc3be85ab73c34b34de43e064c39
jpaulobf <jpaulobf@gmail.com>
2025-02-06 21:29:22 +0000

    Clean code

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.function.Predicate;
-
 /**
  * Demo class application
  */
